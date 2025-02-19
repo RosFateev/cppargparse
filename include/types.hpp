4cf32c861f43ce8cbef506c70690f1039f0c4bea
Rostya <rostis.love.fateev@gmail.com>
2025-02-19 10:37:50 +0100

    is_incrementable trait

--- include/types.hpp
+++ include/types.hpp
@@ -22,6 +22,16 @@ namespace argparse
             template<typename T>
             struct is_string_constructible<T, std::void_t<decltype(T(std::declval<std::string>()))>> : std::true_type
             {};
+
+
+            template<typename T, typename = void>
+            struct is_incrementable : std::false_type
+            {};
+
+            template<typename T>
+            struct is_incrementable<T, std::void_t<decltype(++std::declval<T&>(),
+                                                            std::declval<T&>()++)>> : std::true_type
+            {};
         } // namespace traits
     } // namespace type
 }
